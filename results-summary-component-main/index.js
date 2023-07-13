f66fd3a24641b140e8a19d5898f35f234458c75c
allamvignesh <allam.vignesh@gmail.com>
2023-07-13 21:37:44 +0530

    use data.json for values

--- results-summary-component-main/index.js
+++ results-summary-component-main/index.js
@@ -1,4 +1,17 @@
-const fs = require('fs');
-fs.readFile("./data.json", "utf8", (err, jsonString) => {
-    console.log(jsonString);
+let summary = document.querySelector(".summary")
+
+fetch("./data.json")
+.then(res => res.json())
+.then(result => {
+    for(let i=0; i<result.length; i++){
+        let d = document.createElement('div');
+        d.classList.add("parts");
+        d.classList.add(`${result[i]["color"]}`);
+        d.innerHTML = `<span><img src="${result[i]["icon"]}" alt="${result[i]["category"]} icon">
+        ${result[i]["category"]}
+        </span>
+        <div>${result[i]["score"]} <span>/ 100</span></div>`
+
+        summary.insertBefore(d, document.querySelector(".continue"))
+    }
 })
