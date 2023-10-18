5dc46a828f1a8ba2dc8f4f1a49d94b4b4119209c
allamvignesh <allam.vignesh@gmail.com>
2023-10-18 19:46:50 +0530

    Update

--- Challenges/results-summary-component-main/index.js
+++ Challenges/results-summary-component-main/index.js
@@ -1,6 +1,6 @@
 let summary = document.querySelector(".summary")
 
-fetch("https://raw.githubusercontent.com/allamvignesh/fEndMentor_Challenges/main/results-summary-component-main/data.json")
+fetch("./data.json")
 .then(res => res.json())
 .then(result => {
     for(let i=0; i<result.length; i++){
